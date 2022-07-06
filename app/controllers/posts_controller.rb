38c9902c81dc9f0dda11458d4a7ce6d8e8b01fd6
Nikita Gediya <gediyanikita@gmail.com>
2022-07-06 14:32:09 +0530

    inital commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,24 +1,45 @@
 class PostsController < ApplicationController
     def index
-        @content_first = "This is some sample text for our awesome new Ruby blog";
-        @content_second = "This is some sample text for our awesome new Ruby blog";
+        @posts = Post.all
+        # @content_first = "This is some sample text for our awesome new Ruby blog";
+        # @content_second = "This is some sample text for our awesome new Ruby blog";
+    end
+    def show
+        @post = Post.find(params[:id])
     end
     def new
-        
+        @post = Post.new
+        @category = Category.all
     end
     def create
-        
+        @post = Post.new(post_params)
+       if @post.save
+            # render json: @post, status: :created
+            redirect_to posts_path, :notice => "your post has been saved"
+       else 
+        # render json: @post.errors, status: :unprocessable_entity
+        render "new"
     end
+end
     def edit
+        @post = Post.find(params[:id])
         
     end
     def update
-        
-    end
-    def show
-        
+        @post = Post.find(params[:id])
+        if @post.update(post_params)
+            redirect_to posts_path, :notice => "your post has been updated"
+        else
+            render "edit"
+        end
     end
     def destroy
-        
+        @post = Post.find(params[:id])
+        @post.destroy
+        redirect_to posts_path, :notice => "Your post has been deleted"
     end
+    
+  def post_params
+    params.require(:post).permit(:title, :body ,:category_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'categories/index'
+  get 'categories/edit'
+  get 'categories/new'
+  get 'categories/show'
   # get 'home/index'
   resources :posts
+  resources :categories
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
